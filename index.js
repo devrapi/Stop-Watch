f3af5e10f046ab52e405134a39311c479e393915
Waffy Mawanan <you@example.com>
2024-04-10 22:25:38 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// STOPWATCH PROGRAM
+
+const display = document.getElementById("display");
+let timer = null;
+let StartTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+    if(!isRunning){
+        StartTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - StartTime;
+        isRunning = false;
+
+    }
+}
+
+function reset(){
+    clearInterval(timer);
+    StartTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+}
+
+function update(){
+    
+    const currentTime = Date.now();
+    elapsedTime = currentTime - StartTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60 ) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60 );
+    let milliseconds =Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+
+}
